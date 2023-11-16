78adc4c8ae6207bf4cb61f2b29ca3203a8d76bae
manju <manjunatha3800@gmail.com>
2023-11-16 16:48:03 +0000

    solution

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      className="profile-img"
+      src="https://res.cloudinary.com/manjunatha/image/upload/c_thumb,w_200,g_face/v1668957014/manju/WhatsApp_Image_2022-11-20_at_20.39.20_ejbdq6.jpg"
+      alt="profile"
+    />
+    <h1 className="user-name">Manjunatha v </h1>
+    <p className="user-designation">Software developer at UK</p>
+  </div>
+)
+
+export default UserInfo
